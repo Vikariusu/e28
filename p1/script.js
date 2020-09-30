c2ee7e3fca04ac28a384567f85824cc7098c936f
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-09-29 20:49:02 -0400

    add vue data properties and event listeners to project1

--- p1/script.js
+++ p1/script.js
@@ -1,4 +1,28 @@
 let app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    guess: "",
+    guesses: [],
+    guessCount: 0,
+    secretNum: Math.floor(Math.random() * 100) + 1,
+    message: "",
+  },
+  methods: {
+    submitGuess() {
+      this.guesses.push(this.guess);
+      this.guessCount++;
+      this.handleGuess();
+    },
+    handleGuess() {
+      let text = "";
+      if (this.guess > this.secretNum) {
+        text = "Too high!";
+      } else if (this.guess < this.secretNum) {
+        text = "Too low!";
+      } else {
+        text = `Congrats! The number was ${this.guess}`;
+      }
+      this.message = text;
+    },
+  },
 });
