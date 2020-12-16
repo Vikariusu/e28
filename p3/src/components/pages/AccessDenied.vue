7df16357391151b649eb3e4048605ce9223e47be
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-12-15 23:33:23 -0500

    Add protected routes

---
+++ p3/src/components/pages/AccessDenied.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>
+      You don't have access to view this page, please log in to your account or
+      register a new one.
+    </p>
+  </div>
+</template>
+
+<script></script>
+<style scoped>
+p {
+  text-align: center;
+}
+</style>
--- p3/src/main.js
+++ p3/src/main.js
@@ -1,33 +1,58 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
 
-import HomePage from '@/components/pages/HomePage.vue';
-import RecipeDetail from '@/components/pages/RecipeDetail.vue';
-import AddRecipe from '@/components/pages/AddRecipe.vue';
-import FavoriteList from '@/components/pages/FavoriteList.vue';
-import LoginPage from '@/components/pages/LoginPage.vue';
-import RegisterPage from '@/components/pages/RegisterPage.vue';
+import HomePage from "@/components/pages/HomePage.vue";
+import RecipeDetail from "@/components/pages/RecipeDetail.vue";
+import AddRecipe from "@/components/pages/AddRecipe.vue";
+import FavoriteList from "@/components/pages/FavoriteList.vue";
+import LoginPage from "@/components/pages/LoginPage.vue";
+import RegisterPage from "@/components/pages/RegisterPage.vue";
+import AccessDenied from "@/components/pages/AccessDenied.vue";
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import store from '@/common/store';
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import store from "@/common/store";
 
-import '@fortawesome/fontawesome-free/css/all.css';
-import '@fortawesome/fontawesome-free/js/all.js';
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 Vue.use(VueRouter);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: HomePage },
-    { path: '/add', component: AddRecipe },
-    { path: '/recipes/:id', component: RecipeDetail, props: true },
-    { path: '/favorite', component: FavoriteList },
-    { path: '/login', component: LoginPage },
-    { path: '/register', component: RegisterPage },
+    { path: "/", component: HomePage },
+    { path: "/add", component: AddRecipe },
+    { path: "/recipes/:id", component: RecipeDetail, props: true },
+    {
+      path: "/favorite",
+      component: FavoriteList,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    { path: "/login", component: LoginPage },
+    { path: "/register", component: RegisterPage },
+    {
+      path: "/noaccess",
+      component: AccessDenied,
+    },
   ],
-  mode: 'history',
+  mode: "history",
+});
+
+// Ref: https://router.vuejs.org/guide/advanced/navigation-guards.html
+router.beforeEach(async (to, from, next) => {
+  // Ref: https://router.vuejs.org/guide/advanced/meta.html#route-meta-fields
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (requiresAuth && !store.state.user) {
+    // Protected route - display no access page
+    next("/noaccess");
+  } else {
+    // Display the page if the user has access to it
+    next();
+  }
 });
 
 new Vue({
